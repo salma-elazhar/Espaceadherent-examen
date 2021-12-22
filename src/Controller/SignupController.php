27b4c1ca575dddbe7892a16f6ad6711473ed4508
salma-elazhar <s.lazher2907@myu.universitecentrale.tn>
2021-12-22 22:12:25 +0100

    commit

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -29,8 +29,9 @@ class SignupController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout(): void
+    public function logout():Response
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+
     }
 }
